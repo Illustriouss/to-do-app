19e8a6f71ed3bef9f0311ef1ea5a13a43ed4fa32
Illustriouss <jorishb@hotmail.com>
2017-12-13 14:53:17 +0100

    template now display list items

--- list/views.py
+++ list/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import ListItem
 
 # Create your views here.
 
 def item_list(request):
-	return render(request, 'list/item_list.html')
+	items = ListItem.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'list/item_list.html', {'items': items})
